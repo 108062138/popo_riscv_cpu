daed76bd6c9185a376933cd82568bb799894c651
108062138 <poyuwu17@gmail.com>
2025-09-09 18:05:06 +0800

    stage to debug AXI interface's write channel and its dma

---
+++ src/bus_integrate/bus_one_dma_master_2_one_memory_slave.v
@@ -0,0 +1,260 @@
+module bus_one_dma_master_2_one_memory_slave #(
+    parameter ADDR_WIDTH = 32,
+    parameter READ_CHANNEL_WIDTH = 32,
+    parameter READ_BURST_LEN = 8,
+    parameter WRITE_CHANNEL_WIDTH = 32,
+    parameter WRITE_BURST_LEN = 8,
+    parameter ASYNCFIFO_CHANNEL_WIDTH = 32,
+    parameter ASYNCFIFO_ADDR_WIDTH = 4
+)(
+    input wire cpu_clk,
+    input wire cpu_rst_n,
+    input wire sys_clk,
+    input wire sys_rst_n,
+    // page fault happend
+    input wire dma_page_fault_happen,
+    output wire dma_page_fault_done,
+    input wire [ADDR_WIDTH-1:0] dma_page_fault_addr,
+    input wire [READ_BURST_LEN-1:0] dma_page_fault_burst_len,
+    // cache flush happend
+    input wire dma_write_back_happen,
+    output wire dma_write_back_done,
+    input wire [ADDR_WIDTH-1:0] dma_write_back_addr,
+    input wire [WRITE_BURST_LEN-1:0] dma_write_back_burst_len
+);
+
+// trigger axi read procedure
+wire axi_master_read_start;
+wire axi_master_read_done;
+wire [ADDR_WIDTH-1:0] axi_master_target_read_addr;
+wire [READ_BURST_LEN-1:0] axi_master_target_read_burst_len;
+// read address channel
+wire ARREADY;
+wire [ADDR_WIDTH-1:0] ARADDR;
+wire ARVALID;
+wire [READ_BURST_LEN-1:0] ARLEN;
+wire [2:0] ARSIZE;
+wire [1:0] ARBURST;
+// read data channel
+wire RVALID;
+wire [READ_CHANNEL_WIDTH-1:0] RDATA;
+wire RLAST;
+wire [1:0] RRESP;
+wire RREADY;
+// master2dma_afifo
+wire master2dma_afifo_rpull;
+wire master2dma_afifo_rempty;
+wire [READ_CHANNEL_WIDTH-1:0] master2dma_afifo_rdata;
+wire master2dma_afifo_wpush;
+wire [READ_CHANNEL_WIDTH-1:0] master2dma_afifo_wdata;
+wire master2dma_afifo_wfull;
+
+// trigger axi write procedur
+wire axi_master_write_start;
+wire axi_master_write_done;
+wire [ADDR_WIDTH-1:0] axi_master_target_write_addr;
+wire [WRITE_BURST_LEN-1:0] axi_master_target_write_burst_len;
+// write address channel
+wire AWREADY;
+wire [ADDR_WIDTH-1:0] AWADDR;
+wire AWVALID;
+wire [WRITE_BURST_LEN-1:0] AWLEN;
+wire [2:0] AWSIZE;
+wire [1:0] AWBURST;
+// write data channe
+wire WREADY;
+wire WVALID;
+wire [WRITE_CHANNEL_WIDTH-1:0] WDATA;
+wire WLAST;
+// write data responce channe
+wire BREADY;
+wire BRESP;
+wire BVALID;
+// push data into dma2master_afifo
+wire dma2master_afifo_wpush;
+wire [WRITE_CHANNEL_WIDTH-1:0] dma2master_afifo_wdata;
+wire dma2master_afifo_wfull;
+wire [WRITE_CHANNEL_WIDTH-1:0] dma2master_afifo_rdata;
+wire dma2master_afifo_rpull;
+wire dma2master_afifo_rempty;
+
+// READ DATA: CPU <= DMA <= MASTER <= BUS <= SLAVE <= MEMORY
+asyncfifo #(
+    .DATA_WIDTH(ASYNCFIFO_CHANNEL_WIDTH),
+    .ADDR_WIDTH(ASYNCFIFO_ADDR_WIDTH)
+) u_master2dma_afifo (
+    // read domain is system clock
+    .rclk(cpu_clk),
+    .rrst_n(cpu_rst_n),
+    .rpull(master2dma_afifo_rpull),
+    .rempty(master2dma_afifo_rempty),
+    .rdata(master2dma_afifo_rdata),
+    // write domain: cpu clk
+    .wclk(sys_clk),
+    .wrst_n(sys_rst_n),
+    .wpush(master2dma_afifo_wpush),
+    .wfull(master2dma_afifo_wfull),
+    .wdata(master2dma_afifo_wdata)
+);
+
+// WRITE DATA: CPU => DMA => MASTER => BUS => SLAVE => MEMORY
+asyncfifo #(
+    .DATA_WIDTH(ASYNCFIFO_CHANNEL_WIDTH),
+    .ADDR_WIDTH(ASYNCFIFO_ADDR_WIDTH)
+) u_dma2master_afifo (
+    // read domain is system clock
+    .rclk(sys_clk),
+    .rrst_n(sys_rst_n),
+    .rpull(dma2master_afifo_rpull),
+    .rempty(dma2master_afifo_rempty),
+    .rdata(dma2master_afifo_rdata),
+    // write domain: cpu clk
+    .wclk(cpu_clk),
+    .wrst_n(cpu_rst_n),
+    .wpush(dma2master_afifo_wpush),
+    .wfull(dma2master_afifo_wfull),
+    .wdata(dma2master_afifo_wdata)
+);
+
+dma #(
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .READ_CHANNEL_WIDTH(READ_CHANNEL_WIDTH),
+    .READ_BURST_LEN(READ_BURST_LEN),
+    .WRITE_CHANNEL_WIDTH(WRITE_CHANNEL_WIDTH),
+    .WRITE_BURST_LEN(WRITE_BURST_LEN)
+) u_dma (
+    .cpu_clk(cpu_clk),
+    .cpu_rst_n(cpu_rst_n),
+    // page fault happend
+    .dma_page_fault_happen(dma_page_fault_happen),
+    .dma_page_fault_done(dma_page_fault_done),
+    .dma_page_fault_addr(dma_page_fault_addr),
+    .dma_page_fault_burst_len(dma_page_fault_burst_len),
+    // trigger axi read procedure
+    .axi_master_read_start(axi_master_read_start),
+    .axi_master_read_done(axi_master_read_done),
+    .axi_master_target_read_addr(axi_master_target_read_addr),
+    .axi_master_target_read_burst_len(axi_master_target_read_burst_len),
+    // pull data from master2dma_afifo
+    .master2dma_afifo_rpull(master2dma_afifo_rpull),
+    .master2dma_afifo_rempty(master2dma_afifo_rempty),
+    .master2dma_afifo_rdata(master2dma_afifo_rdata),
+
+    // cache flush happend
+    .dma_write_back_happen(dma_write_back_happen),
+    .dma_write_back_done(dma_write_back_done),
+    .dma_write_back_addr(dma_write_back_addr),
+    .dma_write_back_burst_len(dma_write_back_burst_len),
+    // trigger axi write procedure
+    .axi_master_write_start(axi_master_write_start),
+    .axi_master_write_done(axi_master_write_done),
+    .axi_master_target_write_addr(axi_master_target_write_addr),
+    .axi_master_target_write_burst_len(axi_master_target_write_burst_len),
+    // push data into dma2master_afifo
+    .dma2master_afifo_wpush(dma2master_afifo_wpush),
+    .dma2master_afifo_wdata(dma2master_afifo_wdata),
+    .dma2master_afifo_wfull(dma2master_afifo_wfull)
+);
+
+axi_master #(
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .READ_CHANNEL_WIDTH(READ_CHANNEL_WIDTH),
+    .READ_BURST_LEN(READ_BURST_LEN),
+    .WRITE_CHANNEL_WIDTH(WRITE_CHANNEL_WIDTH),
+    .WRITE_BURST_LEN(WRITE_BURST_LEN)
+) u_axi_master (
+    .clk(sys_clk),
+    .rst_n(sys_rst_n),
+    // read control:
+    .start_read(axi_master_read_start),
+    .target_read_addr(axi_master_target_read_addr),
+    .target_read_burst_len(axi_master_target_read_burst_len),
+    .done_read(axi_master_target_read_burst_len),
+    // read address channel
+    .ARREADY(ARREADY),
+    .ARADDR(ARADDR),
+    .ARVALID(ARVALID),
+    .ARLEN(ARLEN),
+    .ARSIZE(ARSIZE),
+    .ARBURST(ARBURST),
+    // read data channel
+    .RVALID(RVALID),
+    .RDATA(RDATA),
+    .RLAST(RLAST),
+    .RRESP(RRESP),
+    .RREADY(RREADY),
+    // master 2 dma fifo
+    .master2dma_afifo_wpush(master2dma_afifo_wpush),
+    .master2dma_afifo_wdata(master2dma_afifo_wdata),
+    .master2dma_afifo_wfull(master2dma_afifo_wfull),
+
+    
+    // write control:
+    .start_write(axi_master_write_start),
+    .target_write_addr(axi_master_target_write_addr),
+    .target_write_burst_len(axi_master_target_write_burst_len),
+    .done_write(axi_master_write_done),
+    // write address channel
+    .AWREADY(AWREADY),
+    .AWADDR(AWADDR),
+    .AWVALID(AWVALID),
+    .AWLEN(AWLEN),
+    .AWSIZE(AWSIZE),
+    .AWBURST(AWBURST),
+    // write data channel
+    .WREADY(WREADY),
+    .WVALID(WVALID),
+    .WDATA(WDATA),
+    .WLAST(WLAST),
+    // dma 2 master fifo
+    .dma2master_afifo_rdata(dma2master_afifo_rdata),
+    .dma2master_afifo_rpull(dma2master_afifo_rpull),
+    .dma2master_afifo_rempty(dma2master_afifo_rempty),
+    // write data responce channel
+    .BREADY(BREADY),
+    .BRESP(BRESP),
+    .BVALID(BVALID),
+);
+
+axi_slave #(
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .READ_CHANNEL_WIDTH(READ_CHANNEL_WIDTH),
+    .READ_BURST_LEN(READ_BURST_LEN),
+    .WRITE_CHANNEL_WIDTH(WRITE_CHANNEL_WIDTH),
+    .WRITE_BURST_LEN(WRITE_BURST_LEN)
+) u_axi_slave (
+    .clk(sys_clk),
+    .rst_n(sys_rst_n),
+    // read address channel
+    .ARREADY(ARREADY),
+    .ARADDR(ARADDR),
+    .ARVALID(ARVALID),
+    .ARLEN(ARLEN),
+    .ARSIZE(ARSIZE),
+    .ARBURST(ARBURST),
+    // read data channel
+    .RVALID(RVALID),
+    .RDATA(RDATA),
+    .RLAST(RLAST),
+    .RRESP(RRESP),
+    .RREADY(RREADY),
+
+    // write address channel
+    .AWREADY(AWREADY),
+    .AWADDR(AWADDR),
+    .AWVALID(AWVALID),
+    .AWLEN(AWLEN),
+    .AWSIZE(AWSIZE),
+    .AWBURST(AWBURST),
+    // write data channel
+    .WREADY(WREADY),
+    .WVALID(WVALID),
+    .WDATA(WDATA),
+    .WLAST(WLAST),
+    // write responce channel
+    BREADY(BREADY),
+    BRESP(BRESP),
+    BVALID(BVALID)
+);
+
+endmodule
---
+++ src/bus_integrate/dma.v
@@ -0,0 +1,147 @@
+// it manipulate axi-master by start and listen to its end signal
+module dma #(
+    parameter ADDR_WIDTH = 32,
+    parameter READ_CHANNEL_WIDTH = 32,
+    parameter READ_BURST_LEN = 8,
+    parameter WRITE_CHANNEL_WIDTH = 32,
+    parameter WRITE_BURST_LEN = 8
+)(
+    input wire cpu_clk,
+    input wire cpu_rst_n,
+    // page fault happend
+    input wire dma_page_fault_happen,
+    output wire dma_page_fault_done,
+    input wire [ADDR_WIDTH-1:0] dma_page_fault_addr,
+    input wire [READ_BURST_LEN-1:0] dma_page_fault_burst_len,
+    // trigger axi read procedure
+    output reg axi_master_read_start,
+    input axi_master_read_done,
+    output reg [ADDR_WIDTH-1:0] axi_master_target_read_addr,
+    output reg [READ_BURST_LEN-1:0] axi_master_target_read_burst_len,
+    // pull data from master2dma_afifo
+    output reg master2dma_afifo_rpull,
+    input wire master2dma_afifo_rempty,
+    input wire [READ_CHANNEL_WIDTH-1:0] master2dma_afifo_rdata,
+
+    // cache flush happend
+    input wire dma_write_back_happen,
+    output wire dma_write_back_done,
+    input wire [ADDR_WIDTH-1:0] dma_write_back_addr,
+    input wire [WRITE_BURST_LEN-1:0] dma_write_back_burst_len,
+    // trigger axi write procedure
+    output reg axi_master_write_start,
+    input axi_master_write_done,
+    output reg [ADDR_WIDTH-1:0] axi_master_target_write_addr,
+    output reg [WRITE_BURST_LEN-1:0] axi_master_target_write_burst_len,
+    // push data into dma2master_afifo
+    output reg dma2master_afifo_wpush,
+    output reg [WRITE_CHANNEL_WIDTH-1:0] dma2master_afifo_wdata,
+    input wire dma2master_afifo_wfull
+);
+
+localparam r_idle = 0;
+localparam r_processing = 1;
+localparam r_done = 2;
+localparam w_idle = 0;
+localparam w_processing = 1;
+localparam w_done = 2;
+
+reg [1:0] r_state, n_r_state;
+reg [READ_BURST_LEN-1:0] r_cnt, n_r_cnt;
+reg [1:0] w_state, n_w_state;
+reg [WRITE_BURST_LEN-1:0] w_cnt, n_w_cnt;
+
+assign dma_page_fault_done = (r_state == r_done);
+assign dma_write_back_done = (w_state == w_done);
+
+// naively pull data when possible
+always@(*)begin
+    n_r_cnt = r_cnt;
+    master2dma_afifo_rpull = 0;
+    if(r_state==r_idle)begin
+        n_r_cnt = 0;
+    end else begin
+        if(!master2dma_afifo_rempty) begin
+            master2dma_afifo_rpull = 1;
+            n_r_cnt = r_cnt + 1;
+        end
+    end
+end
+// naively push data when possible
+always@(*)begin
+    n_w_cnt = w_cnt;
+    dma2master_afifo_wpush = 0;
+    dma2master_afifo_wdata = 0;
+    if(r_state == w_idle)begin
+        n_w_cnt = 0;
+    end else begin
+        if(!dma2master_afifo_wfull)begin
+            dma2master_afifo_wpush = 1;
+            dma2master_afifo_wdata = dma_write_back_addr + w_cnt;
+            n_w_cnt = w_cnt + 1;
+        end
+    end
+end
+// handle read
+always@(*)begin
+    axi_master_read_start = (r_state == r_processing);
+    axi_master_target_read_addr = dma_page_fault_addr;
+    axi_master_target_read_burst_len = dma_page_fault_burst_len;
+end
+always@(*)begin
+    case(r_state)
+    r_idle:begin
+        if(dma_page_fault_happen) n_r_state = r_processing;
+        else n_r_state = r_idle;
+    end
+    r_processing:begin
+        if(axi_master_read_done) n_r_state = r_done;
+        else n_r_state = r_processing;
+    end
+    r_done:begin
+        if(r_cnt >= dma_page_fault_burst_len) n_r_state = r_idle;
+        else n_r_state = r_done;
+    end
+    default: n_r_state = r_state;
+    endcase
+end
+
+// handle write
+always@(*)begin
+    axi_master_write_start = (r_state == r_processing);
+    axi_master_target_write_addr = dma_write_back_addr;
+    axi_master_target_write_burst_len = dma_write_back_burst_len;
+end
+always@(*)begin
+    case(w_state)
+    w_idle:begin
+        if(dma_write_back_happen) n_w_state = w_processing;
+        else n_w_state = w_idle;
+    end
+    w_processing:begin
+        if(axi_master_write_done) n_w_state = w_done;
+        else n_w_state = w_processing;
+    end
+    w_done:begin
+        if(w_cnt >= dma_write_back_burst_len) n_w_state = w_idle;
+        else n_w_state = w_done;
+    end
+    default: n_r_state = w_state;
+    endcase
+end
+
+always @(posedge cpu_clk) begin
+    if(!cpu_rst_n)begin
+        w_state <= w_idle;
+        w_cnt <= 0;
+        r_state <= r_idle;
+        r_cnt <= 0;
+    end else begin
+        w_state <= n_w_idle;
+        w_cnt <= n_w_cnt;
+        r_state <= n_r_idle;
+        r_cnt <= n_r_cnt;
+    end
+end
+
+endmodule
